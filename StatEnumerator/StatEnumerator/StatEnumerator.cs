1b822b9fe96efe53a3f36261d52692e99c7057d4
TehCheat <thecheatuc@gmail.com>
2018-12-12 08:18:00 -0500

    Initialized repo.

---
+++ StatEnumerator/StatEnumerator/StatEnumerator.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using PoeHUD.Controllers;
+using PoeHUD.Hud.Interfaces;
+using PoeHUD.Hud.Settings;
+using PoeHUD.Hud.UI;
+using PoeHUD.Hud;
+using PoeHUD.Framework;
+using PoeHUD.Framework.Helpers;
+using PoeHUD.Models;
+using PoeHUD.Poe.Components;
+using PoeHUD.Poe.Elements;
+using PoeHUD.Poe.FilesInMemory;
+using PoeHUD.Poe.RemoteMemoryObjects;
+using PoeHUD.Poe;
+using PoeHUD.Plugins;
+
+
+namespace StatEnumerator
+{
+	public class StatEnumerator : BaseSettingsPlugin<StatEnumeratorSettings>
+	{
+		public override void Initialise()
+		{
+			PluginName = "StatEnumerator";
+		}
+
+		public override void Render()
+		{
+			// enumerate stats
+			StreamWriter file = new StreamWriter("GameStats.txt");
+			
+			foreach (var statRecord in GameController.Instance.Files.Stats.records)
+			{
+				file.WriteLine(statRecord.Key);
+			}
+}
+	}
+}
---
+++ StatEnumerator/StatEnumerator/StatEnumeratorSettings.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PoeHUD.Plugins;
+using PoeHUD.Hud.Settings;
+
+namespace StatEnumerator
+{
+	public class StatEnumeratorSettings : SettingsBase
+	{
+		public StatEnumeratorSettings()
+		{
+			Enable = true;
+		}
+	}
+}
